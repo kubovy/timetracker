f56d8364c5c168f32f0b1dd1334b58d4f60feb41
Jan Kubovy <jkubovy@faw.jku.at>
2013-05-14 15:42:50 +0200

    Add log validation and finish entry.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,57 +1,10 @@
 class LogsController < ApplicationController
-	before_action :set_log, only: [:edit, :update, :destroy]
-	before_action :set_data, only: [:new]
+	include TimeHelper
+
+	before_action :set_data
 
   # GET /logs/new
   def new
-    @log = Log.new
-		@logs = Log.where(
-        :date => @selected_date,
-        :employer_id => selected_employer_id,
-        :user_id => current_user.id)
-
-
-    day_diff = (@selected_date.wday - 1 < 0 ? 6 : @selected_date.wday - 1) - 1
-    week_begin = Time.at(@selected_date.to_time.to_i - day_diff * 60 * 60 * 24).utc
-    week_begin = Time.new(week_begin.year, week_begin.month, week_begin.day, 0, 0, 0).utc
-    week_end   = week_begin + 60 * 60 * 24 * 7 - 1
-    week = week_begin..week_end
-
-    month_begin = Time.new(@selected_date.year, @selected_date.month, 1, 0, 0, 0)
-    month_end = Time.new(@selected_date.next_month.year, @selected_date.next_month.month, 1, 0, 0, 0) - 1
-    month = month_begin..month_end
-
-    @daily_total = Log.select("(sum(TIME_TO_SEC(`duration`))) as result")
-		    .where(:employer_id => selected_employer_id, :user_id => current_user.id, :date => @selected_date)
-    @daily_total = @daily_total.first[:result].nil? ? 0 : @daily_total.first[:result]
-
-    @weekly_total = Log.select("(sum(TIME_TO_SEC(`duration`))) as result")
-        .where(:employer_id => selected_employer_id, :user_id => current_user.id, :date => week)
-    @weekly_total = @weekly_total.first[:result].nil? ? 0 : @weekly_total.first[:result]
-
-    @monthly_total = Log.select("(sum(TIME_TO_SEC(`duration`))) as result")
-		    .where(:employer_id => selected_employer_id, :user_id => current_user.id, :date => month)
-    @monthly_total = @monthly_total.first[:result] .nil? ? 0 : @monthly_total.first[:result]
-
-		@timetable = Hash[
-				Timetable.select("day, TIME_TO_SEC(hours) as seconds")
-					.where(:employer_id => selected_employer_id,
-				         :user_id => current_user.id).map{|t|
-							[t.day, t.seconds / 3600]} ]
-		@holidays = Holiday.where(:employer_id => selected_employer_id).map{|holiday| holiday.date}
-
-		@weekly_todo = 0
-    (0..7).each do |i|
-	    day = week_begin + i * 24 * 3600
-	    @weekly_todo += @timetable[day.wday] * 3600 unless @holidays.include?(day) or @timetable[day.wday].nil?
-    end
-
-    @monthly_todo = 0
-		(0..(month_end.day-1)).each do |i|
-			day = month_begin + i * 24 * 3600
-			@monthly_todo += @timetable[day.wday] * 3600 unless @holidays.include?(day) or @timetable[day.wday].nil?
-		end
-
   end
 
   # GET /logs/1/edit
@@ -110,6 +63,60 @@ class LogsController < ApplicationController
 		cookies.permanent[:selected_date] = params[:date] unless params[:date].nil?
 		@selected_date = Date.parse cookies[:selected_date]
 
+		@logs = Log.where(
+				:date => @selected_date,
+				:employer_id => selected_employer_id,
+				:user_id => current_user.id)
+
+		set_log
+		set_calculations
+		set_stats
+	end
+
+	def set_calculations
+		day_diff = (@selected_date.wday - 1 < 0 ? 6 : @selected_date.wday - 1) - 1
+		week_begin = Time.at(@selected_date.to_time.to_i - day_diff * 60 * 60 * 24).utc
+		week_begin = Time.new(week_begin.year, week_begin.month, week_begin.day, 0, 0, 0).utc
+		week_end   = week_begin + 60 * 60 * 24 * 7 - 1
+		week = week_begin..week_end
+
+		month_begin = Time.new(@selected_date.year, @selected_date.month, 1, 0, 0, 0)
+		month_end = Time.new(@selected_date.next_month.year, @selected_date.next_month.month, 1, 0, 0, 0) - 1
+		month = month_begin..month_end
+
+		@daily_total = Log.select("(sum(TIME_TO_SEC(`duration`))) as result")
+		.where(:employer_id => selected_employer_id, :user_id => current_user.id, :date => @selected_date)
+		@daily_total = @daily_total.first[:result].nil? ? 0 : @daily_total.first[:result]
+
+		@weekly_total = Log.select("(sum(TIME_TO_SEC(`duration`))) as result")
+		.where(:employer_id => selected_employer_id, :user_id => current_user.id, :date => week)
+		@weekly_total = @weekly_total.first[:result].nil? ? 0 : @weekly_total.first[:result]
+
+		@monthly_total = Log.select("(sum(TIME_TO_SEC(`duration`))) as result")
+		.where(:employer_id => selected_employer_id, :user_id => current_user.id, :date => month)
+		@monthly_total = @monthly_total.first[:result] .nil? ? 0 : @monthly_total.first[:result]
+
+		@timetable = Hash[
+				Timetable.select("day, TIME_TO_SEC(hours) as seconds")
+				.where(:employer_id => selected_employer_id,
+				       :user_id => current_user.id).map{|t|
+					[t.day, t.seconds / 3600]} ]
+		@holidays = Holiday.where(:employer_id => selected_employer_id).map{|holiday| holiday.date}
+
+		@weekly_todo = 0
+		(0..7).each do |i|
+			day = week_begin + i * 24 * 3600
+			@weekly_todo += @timetable[day.wday] * 3600 unless @holidays.include?(day) or @timetable[day.wday].nil?
+		end
+
+		@monthly_todo = 0
+		(0..(month_end.day-1)).each do |i|
+			day = month_begin + i * 24 * 3600
+			@monthly_todo += @timetable[day.wday] * 3600 unless @holidays.include?(day) or @timetable[day.wday].nil?
+		end
+	end
+
+	def set_stats
 		cookies.permanent[:chart_period] ||= :day
 		cookies.permanent[:chart_period] = params[:chart_period] unless params[:chart_period].nil?
 		@chart_period = cookies[:chart_period]
@@ -136,20 +143,27 @@ class LogsController < ApplicationController
 
 		@stats = {
 				:project =>  Log
-						.select('count(id) as count, project_id')
-						.where(where, current_user.id, selected_employer_id, date_begin, date_end)
-						.group(:project_id).map{ |s|
-								{:project => Project.find(s[:project_id]).name, :count => s[:count]}},
+				.select('count(id) as count, project_id')
+				.where(where, current_user.id, selected_employer_id, date_begin, date_end)
+				.group(:project_id).map{ |s|
+					{:project => Project.find(s[:project_id]).name, :count => s[:count]}},
 				:task =>  Log
-						.select("count(id) as count, task_id")
-						.where(where, current_user.id, selected_employer_id, date_begin, date_end)
-						.group(:task_id).map{ |s|
-								{:task => Task.find(s[:task_id]).name, :count => s[:count]}},
+				.select("count(id) as count, task_id")
+				.where(where, current_user.id, selected_employer_id, date_begin, date_end)
+				.group(:task_id).map{ |s|
+					{:task => Task.find(s[:task_id]).name, :count => s[:count]}},
 		}
 	end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def log_params
+		if params[:log][:finish] then
+			unless parse_time(params[:log][:finish]).nil? then
+				diff = parse_time(params[:log][:finish]) - parse_time(params[:log][:start])
+				params[:log][:duration] = params[:log][:finish] if diff < 0
+				params[:log][:duration] = print_time(diff) unless diff < 0
+			end
+		end
     params.require(:log).permit(:date, :team_id, :client_id, :project_id,
         :task_id, :start, :duration, :description, :billable).merge({
 		    :employer_id => selected_employer_id,
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,3 +1,14 @@
+class TimingValidator < ActiveModel::Validator
+	include TimeHelper
+
+	def validate(record)
+		#diff = parse_time(record.finish.strftime("%H:%M")) - parse_time(record.start.strftime("%H:%M"))
+		#if diff < 0 or parse_time(record.duration.strftime("%H:%M")) < 0 then
+		#	record.errors[:base] << 'Entry finishes before start'
+		#end
+	end
+end
+
 class Log < ActiveRecord::Base
 	belongs_to :employer
 	belongs_to :team
@@ -5,11 +16,23 @@ class Log < ActiveRecord::Base
 	belongs_to :task
 	belongs_to :user
 
+	validates :employer, :presence => true
+	validates :team, :presence => true
+	validates :project, :presence => true
+	validates :task, :presence => true
+	validates :start, :presence => true
+	validates :finish, :presence => true
+	validates :duration, :presence => true
+	validates :description, :presence => true
+	validates_with TimingValidator
+
 	def self.duration(finish)
 		finish - self.start.hour * 60 * 60 - self.start.min * 60 - self.start.sec
 	end
 
 	def finish
-		self.start + self.duration.hour * 60 * 60 + self.duration.min * 60 + self.duration.sec
+		unless self.start.nil? or self.duration.nil? then
+			self.start + self.duration.hour * 60 * 60 + self.duration.min * 60 + self.duration.sec
+		end
 	end
 end
